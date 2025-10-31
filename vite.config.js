5c4ce5173601d8631705067816aee15e269f133f
Bolesław Bondyra <bolek.bondyra@protonmail.com>
2025-10-31 16:02:43 +0100

    changed base back to root

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "strona_proj_oct_25",
+  base: "/",
   plugins: [
     react({
       babel: {
